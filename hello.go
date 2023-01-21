92335062deac3ebe79d3f293e6dbc87e18aa1653
Monfernape <engr.usmankhalil@gmail.com>
2023-01-21 07:38:27 +0500

    First commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main(){
+	fmt.Println("Hello")
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+}
