b84d69e8a3ada0d9e80afc09faf865dd13931b4a
Monfernape <engr.usmankhalil@gmail.com>
2023-01-23 08:03:36 +0500

    learned statis typing and inferring types

--- hello.go
+++ hello.go
@@ -9,8 +9,23 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
+
+	fmt.Printf("conferenceTickets is %T type variable \n", conferenceTickets)
+
 	fmt.Println("Welcome to our booking application conference called", conferenceName, ". Be sure to enjoy")
 	fmt.Printf("%v is responsible for managing all sort of stuff \n", organizer)
 	fmt.Println("We have total of", conferenceTickets, "tickets and", "only", remainingTickets, "seats are available")
+
+	// static typing
+	var userName string
+	var userTickets int
+	// Asking user about his user name
+	userName = "Evan"
+	userTickets = 1
+	fmt.Printf("%v brought %v tickets", userName, userTickets)
+
+	// infering type
+	guestOfHonour := "M. Azeez"
+	fmt.Printf("Our guest of honor is %v", guestOfHonour)
 
 }
