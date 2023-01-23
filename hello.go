8ad495d9a2835fe78c0cd0a943a045e388973b2b
Monfernape <engr.usmankhalil@gmail.com>
2023-01-23 07:49:50 +0500

    fix: applied formatting

--- hello.go
+++ hello.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
+	const organizer = "Usman Khalil"
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 	fmt.Println("Welcome to our booking application conference called", conferenceName, ". Be sure to enjoy")
+	fmt.Printf("%v is responsible for managing all sort of stuff \n", organizer)
+	fmt.Println("We have total of", conferenceTickets, "tickets and", "only", remainingTickets, "seats are available")
 
 }
