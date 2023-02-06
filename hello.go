6c5d760513dc60219cf77f4da232f268fa35feac
Monfernape <engr.usmankhalil@gmail.com>
2023-02-06 08:03:11 +0500

    fix: created add function

--- hello.go
+++ hello.go
@@ -1,38 +1,51 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	const organizer = "Usman Khalil"
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	// const organizer = "Usman Khalil"
+	// var conferenceName = "Go Conference"
+	// const conferenceTickets = 50
+	// var remainingTickets = 50
 
-	fmt.Printf("conferenceTickets is %T type variable \n", conferenceTickets)
+	// fmt.Printf("conferenceTickets is %T type variable \n", conferenceTickets)
 
-	fmt.Println("Welcome to our booking application conference called", conferenceName, ". Be sure to enjoy")
-	fmt.Printf("%v is responsible for managing all sort of stuff \n", organizer)
-	fmt.Println("We have total of", conferenceTickets, "tickets and", "only", remainingTickets, "seats are available")
+	// fmt.Println("Welcome to our booking application conference called", conferenceName, ". Be sure to enjoy")
+	// fmt.Printf("%v is responsible for managing all sort of stuff \n", organizer)
+	// fmt.Println("We have total of", conferenceTickets, "tickets and", "only", remainingTickets, "seats are available")
 
-	// static typing
-	var userName string
-	var userTickets int
-	// Asking user about his user name
-	userName = "Evan"
-	userTickets = 1
-	fmt.Printf("%v brought %v tickets", userName, userTickets)
+	// // static typing
+	// var userName string
+	// var userTickets int
+	// // Asking user about his user name
+	// userName = "Evan"
+	// userTickets = 1
+	// fmt.Printf("%v brought %v tickets", userName, userTickets)
 
-	// infering type
-	guestOfHonour := "M. Azeez"
-	fmt.Printf("Our guest of honor is %v", guestOfHonour)
+	// // infering type
+	// guestOfHonour := "M. Azeez"
+	// fmt.Printf("Our guest of honor is %v", guestOfHonour)
 
-	var guestUser string
+	// var guestUser string
 
-	fmt.Print("Do you want to enter the conference as well? Add your name here")
-	fmt.Scan(&guestUser)
+	// fmt.Print("Do you want to enter the conference as well? Add your name here")
+	// fmt.Scan(&guestUser)
 
-	fmt.Printf("%v thank you. Your name has been registered", guestUser)
+	// fmt.Printf("%v thank you. Your name has been registered", guestUser)
+
+	// Lets do some mathematics
+
+	var firstNumber int
+	var secondNumber int
+
+	fmt.Println("Hey, you wanna do some summation? Give me two numbers and I can add them for you")
+	fmt.Println("Enter first number ")
+	fmt.Scan(&firstNumber)
+
+	fmt.Println("Great, now the second one ")
+	fmt.Scan(&secondNumber)
+
+	var total = firstNumber + secondNumber
+	fmt.Printf("These two numbers sum up to %v", total)
 
 }
